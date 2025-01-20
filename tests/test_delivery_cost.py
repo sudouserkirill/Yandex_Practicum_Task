6905cb54f411b994c466de929eb6cbef697cffe4
sudouserkirill <kirillkulikov1@bk.ru>
2025-01-20 21:45:59 +0300

    Update test_delivery_cost.py

--- tests/test_delivery_cost.py
+++ tests/test_delivery_cost.py
@@ -9,6 +9,11 @@ def test_calculate_delivery_cost_pairwise(distance, size, fragility, workload, e
     assert calculate_delivery_cost(distance, size, fragility, workload) == expected
 
 
+@pytest.mark.parametrize("distance, size, fragility, workload, expected", TestData.distance_boundary_values)
+def test_calculate_delivery_distance_boundary_values(distance, size, fragility, workload, expected):
+    assert calculate_delivery_cost(distance, size, fragility, workload) == expected
+
+
 @pytest.mark.parametrize("distance, size, fragility, workload", TestData.negative_data)
 def test_calculate_delivery_negative(distance, size, fragility, workload):
     try:
@@ -17,8 +22,3 @@ def test_calculate_delivery_negative(distance, size, fragility, workload):
     except ValueError as e:
         print(e)
         assert True
-
-
-@pytest.mark.parametrize("distance, size, fragility, workload, expected", TestData.distance_boundary_values)
-def test_calculate_delivery_distance_boundary_values(distance, size, fragility, workload, expected):
-    assert calculate_delivery_cost(distance, size, fragility, workload) == expected
